ba3057d88649d95f2f5f6bceeb86ea18905fc81d
Farenthigh <naxx.iiax@gmail.com>
2025-11-13 23:41:05 +0700

    Initial Commit

---
+++ LangBite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LangBite
+//
+//  Created by farenthigh on 13/11/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LangBite/LangBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LangBiteApp.swift
+//  LangBite
+//
+//  Created by farenthigh on 13/11/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct LangBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
